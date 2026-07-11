008d0391444dc6ad113100889164263b675a7300
Ranjat Srivastava <ranjatsrivastava@Mac.lan>
2026-07-11 22:18:11 +0530

    Fix build error: missing closing parenthesis in contributions map

--- client/src/pages/Contributions.jsx
+++ client/src/pages/Contributions.jsx
@@ -254,7 +254,7 @@ export default function Contributions() {
                     )}
                   </td>
                 </tr>
-              ))}
+              )))}
             )}
           </tbody>
         </table>
